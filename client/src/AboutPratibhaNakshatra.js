516642cd27c7e85302a292a7680687d3c98b0d1c
DeagoBrowne <89912823+DeagoBrowne@users.noreply.github.com>
2022-07-04 00:10:00 +0100

    created about page component with placeholder text and image. Also created new css file

---
+++ client/src/AboutPratibhaNakshatra.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutPratibhaNakshatra = () => {
+  return (
+    <div>
+      <h1>About Pratibha Nakshatra</h1>
+      <img className="placeholderImg" src="https://pratibhanakshatra.org/public/storage/galleryImages/2/1594904741_1_q25tx5gdEr1cjKV45d-6sw.jpeg" alt="Placeholder" />
+      <p>Placeholder Text</p>
+    </div>
+  );
+};
+
+export default AboutPratibhaNakshatra;
