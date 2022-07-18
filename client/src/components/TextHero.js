f0465886068ee642be22881c78c9b0d213cbcd1f
Monika39-maker <dangolmonika@yahoo.com>
2022-07-18 21:16:53 +0100

    initial draft of text hero component

---
+++ client/src/components/TextHero.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function TextHero({ title, subtitle, link, buttonText, variant }) {
+	return (
+		<div className={`text-hero-${variant}`}>
+			<h2 className={`text-hero-title-${variant}`}>{title}</h2>
+			<h3 className={`text-hero-subtitle-${variant}`}>{subtitle}</h3>
+			<a className={`text-hero-button-${variant}`} type="button" href={link}>
+				{buttonText}
+			</a>
+		</div>
+	);
+}
+
+export default TextHero;
---
+++ stories/TextHero.stories.js
@@ -0,0 +1,28 @@
+import TextHero from "../client/src/components/TextHero.js";
+
+export default {
+    title: "Components/TextHero",
+    component: TextHero,
+    argTypes: {
+        title: { control: "string" },
+        subtitle: { control: "string", defaultValue: "20" },
+        link: { control: "string" },
+        buttonText:{ control: "string" },
+        variant:{ control: "string" },
+
+    },
+};
+
+
+ const Template = (args) => (<TextHero {...args}/>)
+
+ export const Default = Template.bind({})
+ Default.args = {
+    title: "This is a text Hero",
+    subtitle: "In this subtitle you can write a longer text",
+    link: "https://google.com",
+    buttonText:"Open Google",
+    variant:"red",
+
+ }
+
