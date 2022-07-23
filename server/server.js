99e2ead6af29029b1791351ad3b3ed3cefdebbc4
DeagoBrowne <89912823+DeagoBrowne@users.noreply.github.com>
2022-07-23 15:24:09 +0100

    Added endpoints for posting emails

--- server/server.js
+++ server/server.js
@@ -12,6 +12,8 @@ const contactMessage = {
 	message: "Eat my shorts!",
 };
 
+const emails = require("./emails.json");
+
 const port = parseInt(process.env.PORT || "3000");
 
 app.get("/", function (req, res) {
@@ -21,6 +23,19 @@ app.get("/", function (req, res) {
 app.post("/api/new-contact", function (req, res) {
 	res.json(contactMessage);
 });
+
+app.get("/api/email", (req, res) => {
+	res.send(emails);
+});
+
+app.post("/api/email", (req, res) => {
+	let emailsList = req.body;
+	let newEmail = req.body.email;
+	emails.push({
+		"email": newEmail
+	});
+	res.send(emailsList).status(200);
+})
 
 app.listen(port, () => {
 	console.log(`http://localhost:${port}`);
