eabe8a8e0b9053224b13394fe0b15cc3743c11a3
Monika39-maker <dangolmonika@yahoo.com>
2022-07-24 21:06:50 +0100

    added videohero to the home.js file

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,56 +1,11 @@
-// import { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
-import Header from "../components/Header.js";
 import "./home.css";
-import Footer from "../components/Footer.js";
-import Newsletter from "../components/Newsletter";
+import VideoHero from "../components/VideoHero";
 
-import homepageImage from "../images/homepage-image.jpeg";
 
 export function Home() {
-	// const [message, setMessage] = useState("Loading...");
-
-	// useEffect(() => {
-	// 	fetch("/api")
-	// 		.then((res) => {
-	// 			if (!res.ok) {
-	// 				throw new Error(res.statusText);
-	// 			}
-	// 			return res.json();
-	// 		})
-	// 		.then((body) => {
-	// 			setMessage(body.message);
-	// 		})
-	// 		.catch((err) => {
-	// 			console.error(err);
-	// 		});
-	// }, []);
-
-	return (
-		<main role="main">
-			<div>
-				<Header />
-				<h1>Pratibha Nakshatra</h1>
-				{/* <div>
-					<video src={bgVideo} autoPlay loop muted />
-				</div> */}
-				<img src={homepageImage} style={{ width: "80%" }} alt="placeholder" />
-				<p>
-					Pratibha Nakshatra is the fusion of equality, co-existence, respect,
-					“Pure Soul”, positivity, fraternity and consciousness of origin that
-					will take everyone in the path of harmony, love and ultimately
-					achieving ‘Peace’. The scope of “Pratibha Nakshatra (Star Talent)” is
-					immense, in other words it can be a school of conscience, the
-					participants (Child, Young and Adult) will have a chance to explore
-					their origins to gain knowledge about their culture, attires,
-					traditions, language, arts, music, folklore etc so that they can
-					preserve, maintain and handover to their newer generation.
-				</p>
-			</div>
-			<Newsletter />
-			<Footer />
-		</main>
-	);
+	return <main role="main">
+		<VideoHero />
+	</main>;
 }
 
 export default Home;
