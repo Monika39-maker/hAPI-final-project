18096a8e13f3e3d5ee9294329727bc15166406be
Lorenzo Turrino <neuronefunzionante@gmail.com>
2022-07-24 23:06:51 +0100

    clean up console.log

--- server/server.js
+++ server/server.js
@@ -7,8 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-console.log("__dirname", path.join(__dirname, "public"));
-
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 const contactMessage = {
