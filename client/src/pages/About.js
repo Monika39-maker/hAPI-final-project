6f4008bdb7fa001ad3c747a65ca378812dc77fd8
Monika39-maker <dangolmonika@yahoo.com>
2022-07-25 23:33:03 +0100

    recopied the codes from AboutPratibhsNakshtra to About.js since realized that it was not copied in the previous commit

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,10 +1,14 @@
+import React from "react";
+import Placeholder from "./images/PlaceholderImage.jpeg";
 
-const About = () => (
-	<main role="main">
+const About = () => {
+	return (
 		<div>
-
+			<h1>About Pratibha Nakshatra</h1>
+			<img className="placeholderImg" src={Placeholder} alt="Placeholder" />
+			<p>Placeholder Text</p>
 		</div>
-	</main>
-);
+	);
+};
 
 export default About;
