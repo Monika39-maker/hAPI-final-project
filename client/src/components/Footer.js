407bac149ab913a48971ff42c8bcd7d42d68b17e
Monika39-maker <dangolmonika@yahoo.com>
2022-07-26 18:51:48 +0100

    edited Footer.js to have only the contents as suggested in Figma

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,21 +1,21 @@
 import React from "react";
 import Nav from "./Nav";
-// import { FaInstagramSquare } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
 export default function Footer() {
   return (
 		<div>
+			<p>MENU</p>
 			<Nav />
+			<p>Pratibha Nakshtra © 2022</p>
 			<div>
-				<p>Address: 33 Chancery Lane</p>
-				<p>Phone No: 02094536722</p>
-				<p>Email: blah-blah@gmail.com</p>
-			</div>
-			<div>
-				{/* <a href="">
+				<a href="">
 					<FaInstagramSquare />
-				</a> */}
-				<a href="https://www.facebook.com/prativanakshatra" target="_blank" rel="noreferrer">
+				</a>
+				<a
+					href="https://www.facebook.com/prativanakshatra"
+					target="_blank"
+					rel="noreferrer"
+				>
 					<FaFacebookF />
 				</a>
 			</div>
