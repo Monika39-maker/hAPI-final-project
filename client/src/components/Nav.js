db2b6a2b0a433dd5cdb999a8f4610eddf55d1e3e
Monika39-maker <dangolmonika@yahoo.com>
2022-07-26 01:14:35 +0100

    edited Nav.js file for proper links

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,11 +2,26 @@ import React from "react";
 
 export default function Nav() {
   return (
-    <nav>
-        <li><a href="https://www.pratibhanakshatra.org/public/storage/setting/1594904931pratibha.jpg">Home</a></li>
-        <li><a href="https://www.pratibhanakshatra.org/public/storage/setting/1594904931pratibha.jpg">About</a></li>
-        <li><a href="https://www.pratibhanakshatra.org/public/storage/setting/1594904931pratibha.jpg">Blogs</a></li>
-        <li><a href="https://www.pratibhanakshatra.org/public/storage/setting/1594904931pratibha.jpg">Contact</a></li>
-    </nav>
-  );
+		<nav>
+			<li>
+				<a href="/Home">Home</a>
+			</li>
+			<li>
+				<a href="/About">About</a>
+			</li>
+			<li>
+				<a href="/Home">
+          {/* src needs to be fetched from database  */}
+					<img src="https://www.pratibhanakshatra.org/public/storage/setting/1594904931pratibha.jpg" />
+				</a>
+			</li>
+			<li>
+        {/* href to be changed to other source  */}
+				<a href="https://www.pratibhanakshatra.org/event-list">Events</a>
+			</li>
+			<li>
+				<a href="/Donate">Donate</a>
+			</li>
+		</nav>
+	);
 }
