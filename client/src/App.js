89c086c9b610b1c991dc878698ec02f6880de186
Monika39-maker <dangolmonika@yahoo.com>
2022-07-26 18:16:00 +0100

    fixed error of not importing header and footer at app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,22 @@ import About from "./pages/About";
 import Donations from "./Donations";
 import ThankYou from "./pages/ThankYou";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Contact from "./Contact";
-
-
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => (
-<>
-	<Header />
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about" element={<About />} />
-		<Route path="/donations" element={<Donations />} />
-		<Route path="/thank-you" element={<ThankYou /> }  />
-		<Route path="/contact" element={<Contact />} />
-	</Routes>
-	<Footer />
-  </>
+	<>
+		<Header />
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/about" element={<About />} />
+			<Route path="/donations" element={<Donations />} />
+			<Route path="/thank-you" element={<ThankYou /> }  />
+			<Route path="/contact" element={<Contact />} />
+		</Routes>
+		<Footer />
+	</>
 );
 
 export default App;
