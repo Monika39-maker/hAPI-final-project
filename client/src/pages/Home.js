3bcfcfc1e71da9737d566c97f9d050562ac1cf8d
Monika39-maker <dangolmonika@yahoo.com>
2022-07-29 01:08:24 +0100

    z-index reduced for videohero

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,7 +10,7 @@ export function Home() {
 	return (
 		<main role="main">
 			<VideoHero
-				src="http://localhost:3100/public/videohero.mp4"
+				src="https://pratibha-nakshtra.s3.eu-west-2.amazonaws.com/videohero.mp4"
 				height="50"
 				width="100"
 				textOverlay="Pratibha Nakshatra"
