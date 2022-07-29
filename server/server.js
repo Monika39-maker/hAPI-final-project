301c6216e93477ebc69e168035f01eb6574b58c0
DeagoBrowne <89912823+DeagoBrowne@users.noreply.github.com>
2022-07-29 11:16:41 +0100

    reverted requests from email endpoints to match contacts

--- server/server.js
+++ server/server.js
@@ -32,47 +32,18 @@ app.post("/api/new-contact", function (req, res) {
 	res.json(contactMessage);
 });
 
-app.get("/api/email", function (req, res) {
-	pool
-		.query("SELECT * FROM mailing_list")
-		.then((result) => res.json(result.rows))
-		.catch((error) => {
-			console.error(error);
-			res.status(500).json(error);
-		});
-});
-// app.get("/api/email", (req, res) => {
-// 	res.send(emails);
-// });
-
-app.post("/api/email", function (req, res) {
-	const email = req.body.email;
-
-	if ((!email)) {
-		return res.status(400).send("Please provide valid email.");
-	}
-
-	const query =
-		"INSERT INTO mailing_list (email) VALUES ($1)";
-
-	pool
-		.query(query, [email])
-		.then(() => res.send("email address added to mailing list!"))
-		.catch((error) => {
-			console.error(error);
-			res.status(500).json(error);
-		});
+app.get("/api/email", (req, res) => {
+	res.send(emails);
 });
 
-
-// app.post("/api/email", (req, res) => {
-// 	let emailsList = req.body;
-// 	let newEmail = req.body.email;
-// 	emails.push({
-// 		"email": newEmail,
-// 	});
-// 	res.send(emailsList).status(200);
-// });
+app.post("/api/email", (req, res) => {
+	let emailsList = req.body;
+	let newEmail = req.body.email;
+	emails.push({
+		"email": newEmail,
+	});
+	res.send(emailsList).status(200);
+});
 
 app.listen(port, () => {
 	console.log(`http://localhost:${port}`);
