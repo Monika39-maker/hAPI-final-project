0a9a4aa9a89d9002b7bb2bbac9bc90807ddf0ae6
Monika39-maker <dangolmonika@yahoo.com>
2022-08-05 01:06:52 +0100

    trying to fix routing

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ const App = () => (
 	<>
 		<Header />
 		<Routes>
-			<Route path="/" element={<Home />} />
+			{/* <Route path="/" element={<Home />} /> */}
 			<Route path="/about" element={<About />} />
 			<Route path="/donations" element={<Donations />} />
 			<Route path="/thank-you" element={<ThankYou /> }  />
